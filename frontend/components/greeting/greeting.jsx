65f76dc345a7e2687075ff7b16d78e731e6c63b8
mnsapits <mnsapits@umich.edu>
2016-11-01 16:44:34 -0700

    update greeting forms

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router';
 
 const sessionLinks = () => (
   <nav className="login-signup">
-    <Link to="/login" activeClassName="current">Login</Link>
+    <Link to="/login" activeClassName="current">Sign in</Link>
     &nbsp;or&nbsp;
-    <Link to="/signup" activeClassName="current">Sign up!</Link>
+    <Link to="/signup" activeClassName="current">Sign Up</Link>
   </nav>
 );
 
